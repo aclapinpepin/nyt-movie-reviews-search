456dff209e4e8207d91a4a94a92e1bb0f7200b03
aclapinpepin <aclapinpepin@gmail.com>
2015-10-17 13:57:01 +0200

    Refactor searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,8 +2,10 @@ require 'movie_reviews'
 
 class SearchesController < ApplicationController
   def show
-    # binding.pry
-    @movie = MovieReviews.new
-    @reviews = @movie.reviews(query: {'query' => params[:search][:movie_query], 'thousand-best' => 'N'})
+    @reviews = MovieReviews.new.reviews search_options 
+  end
+
+  def search_options
+    {query: {'query' => params[:search][:movie_query], 'thousand-best' => 'N'} }
   end
 end
